d690dd3e1fc85a931d0174d035ce90b52f6e909a
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2017-07-05 19:54:59 -0700

    init

--- src/srl.py
+++ src/srl.py
@@ -1,6 +1,6 @@
 from dynet import *
 from utils import read_conll
-import time, random, os
+import time, random, os,math
 import numpy as np
 from collections import  defaultdict
 
@@ -48,12 +48,12 @@ class SRLLSTM:
 
         self.inp_dim = self.d_w + self.d_l + self.d_pos + (self.edim if self.external_embedding is not None else 0) + (1 if self.region else 0)  # 1 for predicate indicator
         self.deep_lstms = BiRNNBuilder(self.k, self.inp_dim, 2*self.d_h, self.model, VanillaLSTMBuilder)
-        self.x_re = self.model.add_lookup_parameters((len(self.words) + 2, self.d_w))
-        self.x_le = self.model.add_lookup_parameters((len(self.pred_lemmas) + 2, self.d_l))
-        self.x_pos = self.model.add_lookup_parameters((len(pos), self.d_pos))
-        self.u_l = self.model.add_lookup_parameters((len(self.pred_lemmas) + 2, self.d_prime_l))
-        self.v_r = self.model.add_lookup_parameters((len(self.roles), self.d_r))
-        self.U = self.model.add_parameters((self.d_h * 4, self.d_r + self.d_prime_l))
+        self.x_re = self.model.add_lookup_parameters((len(self.words) + 2, self.d_w), UniformInitializer(1.0/math.sqrt(len(self.words))))
+        self.x_le = self.model.add_lookup_parameters((len(self.pred_lemmas) + 2, self.d_l),UniformInitializer(1.0/math.sqrt(len(self.pred_lemmas))))
+        self.x_pos = self.model.add_lookup_parameters((len(pos), self.d_pos), UniformInitializer(1.0/math.sqrt(len(pos))))
+        self.u_l = self.model.add_lookup_parameters((len(self.pred_lemmas) + 2, self.d_prime_l), UniformInitializer(1.0/math.sqrt(self.d_prime_l)))
+        self.v_r = self.model.add_lookup_parameters((len(self.roles), self.d_r), UniformInitializer(1.0/math.sqrt(self.d_r)))
+        self.U = self.model.add_parameters((self.d_h * 4, self.d_r + self.d_prime_l),UniformInitializer(1.0/math.sqrt(self.d_r + self.d_prime_l)))
         self.empty_lemma_embed = inputVector([0]*self.d_l)
 
     def Save(self, filename):
