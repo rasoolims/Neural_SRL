ea6b98f283b29372e154798f443165a1132dd91b
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2017-11-29 09:58:15 -0500

    fixed the problem with concatenate

--- src/srl.py
+++ src/srl.py
@@ -102,17 +102,10 @@ class SRLLSTM:
     def decode(self, minibatches):
         outputs = [list() for _ in range(len(minibatches))]
         for b, batch in enumerate(minibatches):
-            x = concatenate_cols(self.buildGraph(batch, False))
-            outputs[b] = x.npvalue()
-            print x.dim(), outputs[b].shape
+            outputs[b] = concatenate_cols(self.buildGraph(batch, False)).npvalue()
             renew_cg()
-        print '->',len(outputs)
-        y = np.concatenate(outputs, axis=1)
-        print y.shape
-        z = y.T
-        print z.shape
-        return z
-        #return transpose(concatenate_cols(outputs))
+        outputs = np.concatenate(outputs, axis=1)
+        return outputs.shape
 
     def Train(self, mini_batches):
         print 'Start time', time.ctime()
