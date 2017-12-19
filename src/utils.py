af1799e00a9c095a595155a47235e47eb8c35308
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2017-12-19 11:11:25 -0500

    DEBUGGING

--- src/utils.py
+++ src/utils.py
@@ -148,6 +148,10 @@ def add_to_minibatch(batch, pred_ids, cur_c_len, cur_len, mini_batches, model):
     pred_flags = np.array([np.array([(1 if pred_ids[i][1] == j else 0) if j < len(batch[i]) else 0 for i in range(len(batch))]) for j in range(cur_len)])
     pred_lemmas = np.array([model.pred_lemmas.get(batch[i][pred_ids[i][1]].lemma, 0) for i in range(len(batch))])
     pred_lemmas_index = np.array([pred_ids[i][1] for i in range(len(batch))])
+    for j in range(cur_len):
+        for i in range(len(pred_ids)):
+            model.roles.get(batch[i][j].predicateList[pred_ids[i][0]], 0) if j < len(pred_ids[i]) else model.PAD
+            
     roles = np.array([np.array(
         [model.roles.get(batch[i][j].predicateList[pred_ids[i][0]], 0) if j < len(pred_ids[i]) else model.PAD for i in
          range(len(pred_ids))]) for j in range(cur_len)])
